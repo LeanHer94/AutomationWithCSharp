be064a0ffb835748dd30177debdb637847fb1f9d
Leandro Hereñú <leandrohere94@live.com.ar>
2019-05-26 15:31:13 -0300

    notification sender

---
+++ AutomationWithCSharp/Letters/Services/Interfaces/INotificationSender.cs
@@ -0,0 +1,10 @@
+﻿using AutomationWithCSharp.Letters.Model;
+using System.Collections.Generic;
+
+namespace AutomationWithCSharp.Letters.Services.Interfaces
+{
+    public interface INotificationSender
+    {
+        void Send(string notification, IEnumerable<Person> receivers);
+    }
+}
